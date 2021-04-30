1cdb6dc5488bc8c3d7ca2d81a8559bfb5c64b8b5
Alen <akchurin_alen@mail.ru>
2021-04-30 13:13:11 +0600

    Добавьте файлы проекта.

---
+++ WPFEXAM/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+using WPFEXAM;
+namespace WPFEEXAM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window1
+    {
+       
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+       
+        private void Click1(object sender, RoutedEventArgs e)
+        {
+            Navigate(new Uri(@"C:\Users\Admin\source\repos\WPFEXAM\WPFEXAM\Window1.xaml", UriKind.Relative));
+        }
+
+        private void Navigate(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+
+    
+    }
+}
